c390a675b35ab4e4b9c224a37cbfa9203aa79ab1
shraddha <shra57@gmail.com>
2023-02-12 15:58:18 +0100

    Added gradle nature, java nature and sample java programs

---
+++ src/main/com/independant/programs/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.independant.programs;
+import java.util.Scanner;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the number of terms: ");
+    int n = input.nextInt();
+
+    int a = 0, b = 1, c;
+    System.out.print(a + " " + b);
+    for (int i = 2; i < n; i++) {
+      c = a + b;
+      System.out.print(" " + c);
+      a = b;
+      b = c;
+    }
+    System.out.println();
+  }
+}
